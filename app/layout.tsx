d82bf1457b3b956c4c6e15b7707c591df89d9a5f
Bohdan Moiseienko <bogdan.nadgobb@gmail.com>
2023-12-05 11:53:03 +0100

    Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+interface IRootLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function RootLayout({ children, }: IRootLayoutProps) {
+    return (
+        <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
